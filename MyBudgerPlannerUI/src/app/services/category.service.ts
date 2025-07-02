8e49bceae06fcd8581650991ceab10d22a2df01e
MyCodeClub <ankitsahay1990@gmail.com>
2025-07-02 23:25:43 +0530

    updated dotnet launch from mac & few URL in angular app

--- MyBudgerPlannerUI/src/app/services/category.service.ts
+++ MyBudgerPlannerUI/src/app/services/category.service.ts
@@ -8,16 +8,12 @@ import { UserRegistrationModel, RegistrationResponseModel } from '../models/User
 export class CategoryService {
    constructor(private http: HttpClient) { }
   private _urls = {
-    loginUrl: 'http://mbp.bitprosofttech.com/api/UserAccount/Login',
-    registerUrl: 'http://mbp.bitprosofttech.com/api/UserAccount/UserRegistration',
     categoriesUrl: 'http://mbp.bitprosofttech.com/api/Categories',
 
   };
   
   loginUser(credentials: { email: string; password: string }): Observable<any> {
-    var loginRequest = this.http.get<any>(this._urls.categoriesUrl);
-    console.log('Login request:', loginRequest);
-    return loginRequest;
-  }
+     return this.http.get<any>(this._urls.categoriesUrl);
+   }
 }
  
