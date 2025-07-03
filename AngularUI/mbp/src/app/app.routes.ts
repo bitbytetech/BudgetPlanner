00ffa2779726a9e95ba65687d3916afa45c68cb4
GammingPC_Ankit <ankitsahay1990@gmail.com>
2025-07-03 20:11:53 +0530

    push new ng project in ng 20

---
+++ AngularUI/mbp/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { Categories } from './pages/categories/categories';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+
+	export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'login', component: Login},
+    {path: 'register', component: Register},
+    {path: 'categories', component: Categories}
+	]; 
