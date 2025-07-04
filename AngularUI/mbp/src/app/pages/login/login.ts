110449048638128f3b582e748267d8c14c5b048f
GammingPC_Ankit <ankitsahay1990@gmail.com>
2025-07-04 12:10:24 +0530

    updated login UI

--- AngularUI/mbp/src/app/pages/login/login.ts
+++ AngularUI/mbp/src/app/pages/login/login.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+ 
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.scss'
+    selector: 'app-login',
+    imports: [CommonModule, ReactiveFormsModule, RouterModule],
+    templateUrl: './login.html',
+    styleUrl: './login.scss',
+    standalone: true,
 })
 export class Login {
+  loginForm: FormGroup;
+  notification = signal<string | null>(null);
+  isSuccess = signal(false);
+  isLoading = signal(false);
 
+  constructor(private fb: FormBuilder,   private router: Router) {
+    this.loginForm = this.fb.group({
+      loginName: ['', [Validators.required, Validators.email]],
+      password: ['',
+        [Validators.required,
+        Validators.minLength(6),
+        ]]
+    });
+  }
+
+    onSubmit() {}
 }
