7a76ad89f34daac36050d6ab124bf1d6aeb82ea9
GammingPC_Ankit <ankitsahay1990@gmail.com>
2025-09-03 16:51:11 +0530

    added routing in app.routs.ts
     -- updating url is triggering view

--- AngularUI/my-material-app/src/app/app.routes.ts
+++ AngularUI/my-material-app/src/app/app.routes.ts
@@ -1,5 +1,24 @@
+
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { ExpenseCategories } from './pages/expense-categories/expense-categories';
+import { MonthlyExpensePlan } from './pages/monthly-expense-plan/monthly-expense-plan';
+import { MonthlyExpenses } from './pages/monthly-expenses/monthly-expenses';
+import { MonthlyIncome } from './pages/monthly-income/monthly-income';
+import { NotFound } from './pages/not-found/not-found';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
-    
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: Home },
+    { path: 'about', component: About },
+    { path: 'monthly-income', component: MonthlyIncome },
+    { path: 'monthly-expense-plan', component: MonthlyExpensePlan },
+    { path: 'monthly-expenses', component: MonthlyExpenses },
+    { path: 'expense-categories', component: ExpenseCategories },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: '**', component: NotFound },
 ];
