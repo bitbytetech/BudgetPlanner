d686d4d5ff5084f6cb996c232be23f916a3d0b6c
GammingPC_Ankit <ankitsahay1990@gmail.com>
2025-09-03 17:33:17 +0530

    added login and registration material UI

--- AngularUI/my-material-app/src/app/pages/login/login.ts
+++ AngularUI/my-material-app/src/app/pages/login/login.ts
@@ -1,11 +1,50 @@
+
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    CommonModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  loginForm: FormGroup;
+  errorMessage: string = '';
 
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) {
+    this.loginForm = this.fb.group({
+      loginName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.auth.loginUser(this.loginForm.value).subscribe({
+        next: (res) => {
+          if (res.isLoginSuccess) {
+            this.router.navigate(['/home']);
+          } else {
+            this.errorMessage = 'Login failed. Please check your credentials.';
+          }
+        },
+        error: () => {
+          this.errorMessage = 'Login failed. Please try again.';
+        }
+      });
+    }
+  }
 }
--- AngularUI/my-material-app/src/app/pages/register/register.ts
+++ AngularUI/my-material-app/src/app/pages/register/register.ts
@@ -1,11 +1,60 @@
+
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    CommonModule
+  ],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
+  registerForm: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
 
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastLame: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const user = {
+        ...this.registerForm.value,
+        roles: ['user']
+      };
+      this.auth.registerUser(user).subscribe({
+        next: (res) => {
+          if (res.isCreated) {
+            this.successMessage = 'Registration successful!';
+            this.router.navigate(['/home']);
+          } else {
+            this.errorMessage = 'Registration failed.';
+          }
+        },
+        error: () => {
+          this.errorMessage = 'Registration failed. Please try again.';
+        }
+      });
+    }
+  }
 }
