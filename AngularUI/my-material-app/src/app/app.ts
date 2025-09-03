0c43f8c7e672c2d3ee1e6280bc8a250c4c79e898
GammingPC_Ankit <ankitsahay1990@gmail.com>
2025-09-03 17:06:14 +0530

    Added some required models, series & interceptor

--- AngularUI/my-material-app/src/app/app.ts
+++ AngularUI/my-material-app/src/app/app.ts
@@ -1,12 +1,25 @@
+
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MatToolbarModule,
+    MatButtonModule,
+    CommonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my-material-app');
+  isLoggedIn = false; // placeholder for login state
+  userName = 'User'; // placeholder for user name
 }
