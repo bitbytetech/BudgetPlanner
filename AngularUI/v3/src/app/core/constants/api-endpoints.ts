908d817710e0ddc3c7fe8f3ebaaf8afb13968a4b
MyCodeClub_MacBook4 <ankitsahay1990@gmail.com>
2025-09-13 02:30:53 +0530

    updating layout

--- AngularUI/v3/src/app/core/constants/api-endpoints.ts
+++ AngularUI/v3/src/app/core/constants/api-endpoints.ts
@@ -1,6 +1,7 @@
 // src/app/core/constants/api-endpoints.ts
 
-export const ApiBaseUrl = 'https://localhost:7255/api';
+//export const ApiBaseUrl = 'https://localhost:7255/api'; // windows base url local
+export const ApiBaseUrl = 'http://localhost:5109/api'; // mac base url local
 export const ApiSwagger = 'http://mbp.bitprosofttech.com/swagger/index.html';
 //export const ApiBaseUrl = 'http://mbp.bitprosofttech.com/api';
 export const ApiEndpoints = {
