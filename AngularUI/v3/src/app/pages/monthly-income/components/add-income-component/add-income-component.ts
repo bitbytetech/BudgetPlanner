fcebb771ba1a776a80b0b91860515131bb64a598
AnkitM4Pro <ankit.netcore@gmail.com>
2025-09-19 17:47:32 +0530

    Making simple api call to save data, within single file. -- In Angular, HttpClient methods (get, post, put, etc.) return an Observable that is lazy — nothing happens until you subscribe()

--- AngularUI/v3/src/app/pages/monthly-income/components/add-income-component/add-income-component.ts
+++ AngularUI/v3/src/app/pages/monthly-income/components/add-income-component/add-income-component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'add-income-component',
@@ -14,22 +16,39 @@ export class AddIncomeComponent {
     incomeAmount: new FormControl(0, [Validators.required, Validators.min(1)])
   });
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
     this.incomeForm = this.fb.group({
       sourceName: ['', Validators.required],
       incomeAmount: [0, [Validators.required, Validators.min(1)]]
     });
   }
 
+
+
+
   onSubmit() {
     if (this.incomeForm.valid) {
-      const formData = this.incomeForm.value;
-      console.log('Form Submitted:', formData);
-      // Here you can handle the form submission, e.g., send the data to a server
+      var data = this.http.post<any>("https://localhost:7255/api/IncomeSource/CreateOrEdit", this.incomeForm.value).pipe(
+        tap(response => {
+          console.log(response);
+        })
+      );
+      data.subscribe();
     } else {
       console.log('Form is invalid');
     }
   }
+
+
+  // SaveIncome(credentials: { loginName: string; password: string }): Observable<LoginResponseModel> {
+  //   return this.http.post<LoginResponseModel>(ApiEndpoints.userAccount.login, credentials).pipe(
+  //     tap(response => {
+  //       if (response?.isLoginSuccess) {
+  //         this.setUser(response);
+  //       }
+  //     })
+  //   );
+  // }
 
 }
 
